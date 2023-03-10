9855944cd28c1e7acd0c9aa49de6b7f0c56eb587
shiva <shiva@nimapinfotech.com>
2023-03-10 10:34:01 +0530

    ui change

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,7 +8,7 @@ const Form = () => {
   const [emailErr, setEmailErr] = useState(false);
   const [contact, setContact] = useState("");
   const [contactErr, setContactErr] = useState(false);
-  const[btnBg,setBtnBg]=useState("#67bdfc")
+  const [btnBg, setBtnBg] = useState("#67bdfc");
 
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -36,7 +36,7 @@ const Form = () => {
   const getContact = (e) => {
     setContact(e.target.value);
 
-    if (contact.length != 9 || isNaN(contact)|| contactRegex.test(contact)) {
+    if (contact.length != 9 || isNaN(contact) || contactRegex.test(contact)) {
       setContactErr(true);
     } else {
       setContactErr(false);
@@ -46,55 +46,71 @@ const Form = () => {
 
   function validate() {
     if (name && email && contact && !emailErr && !nameErr && !contactErr) {
-      setBtnBg("#2cef52")
+      setBtnBg("#2cef52");
       alert("form Submitted");
       setEmail("");
       setName("");
       setContact("");
-      
     } else {
       alert("enter complete Details");
     }
-   
   }
 
   return (
     <>
       <div className="main">
-        <input
-          placeholder="Enter Your Name"
-          onChange={(e) => getName(e)}
-          value={name}
-          type="text"
-        />
-        {nameErr && (
-          <p style={{ color: "red", fontSize: "12px" }}>
-            Name length should be between 3 to 10
-          </p>
-        )}
-        <input
-          placeholder="Enter Your email"
-          onChange={getEmail}
-          value={email}
-          type="text"
-        />
-        {emailErr && (
-          <p style={{ color: "red", fontSize: "12px" }}>
-            Enter Correct Email id
-          </p>
-        )}
-        <input
-          placeholder="Enter Your Contact Number"
-          onChange={getContact}
-          type="text"
-          value={contact}
-        />
-        {contactErr && (
-          <p style={{ color: "red", fontSize: "12px" }}>
-            Enter 10Digit Correct Mobile Number
-          </p>
-        )}
-        <button style={{backgroundColor:`${btnBg}`}} onClick={validate} className="submit">
+        <div className="field-div">
+          <label htmlFor="name">Name : </label>
+          <input
+            id="name"
+            placeholder="Enter Your Name"
+            onChange={(e) => getName(e)}
+            value={name}
+            type="text"
+          />
+          {nameErr && (
+            <p style={{ color: "red", fontSize: "12px" }}>
+              Name length should be between 3 to 10
+            </p>
+          )}
+        </div>
+
+        <div className="field-div">
+          <label htmlFor="email">Email id : </label>
+          <input
+            id="email"
+            placeholder="Enter Your email"
+            onChange={getEmail}
+            value={email}
+            type="text"
+          />
+          {emailErr && (
+            <p style={{ color: "red", fontSize: "12px" }}>
+              Enter Correct Email id
+            </p>
+          )}
+        </div>
+
+        <div className="field-div">
+          <label htmlFor="contact">Contact Number : </label>
+          <input
+            id="contact"
+            placeholder="Enter Your Contact Number"
+            onChange={getContact}
+            type="text"
+            value={contact}
+          />
+          {contactErr && (
+            <p style={{ color: "red", fontSize: "12px" }}>
+              Enter 10Digit Correct Mobile Number
+            </p>
+          )}
+        </div>
+
+        <button
+          onClick={validate}
+          className="submit"
+        >
           Submit
         </button>
       </div>
